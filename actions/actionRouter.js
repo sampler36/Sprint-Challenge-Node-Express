438c15f190dc5e59e0094cc461fc3ead0455420d
Talent Antonio <antonio.talenty@gmail.com>
2019-03-29 10:27:23 +0000

    get() Endpoint for actions and projects

--- actions/actionRouter.js
+++ actions/actionRouter.js
@@ -4,4 +4,16 @@ const router = express.Router();
 
 router.use(express.json());
 
+
+ // get 
+ router.get("/", (req, res) => {
+    Actions.get()
+      .then((data) => res.json(data))
+      .catch((data) =>
+        res.status(500).json({
+          errorMessage: "if you see this you way over your head"
+        })
+      );
+  });
+ 
 module.exports = router;
--- projects/projectRouter.js
+++ projects/projectRouter.js
@@ -4,4 +4,15 @@ const router = express.Router();
 
 router.use(express.json());
 
+router.get("/", (req, res) => {
+    Projects.get()
+      .then((data) => res.json(data))
+      .catch((data) =>
+        res.status(500).json({
+          errorMessage: "Go back mate !!"
+        })
+      );
+  });
+ 
+
 module.exports = router;
