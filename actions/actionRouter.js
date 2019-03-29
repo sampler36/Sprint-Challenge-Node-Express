46dbfd8d8140926f27aa05c3105080205a233dea
Talent Antonio <antonio.talenty@gmail.com>
2019-03-29 11:20:31 +0000

    Insert End point for both working

--- actions/actionRouter.js
+++ actions/actionRouter.js
@@ -4,16 +4,28 @@ const router = express.Router();
 
 router.use(express.json());
 
+// get
+router.get("/", (req, res) => {
+  Actions.get()
+    .then((data) => res.json(data))
+    .catch((data) =>
+      res.status(500).json({
+        errorMessage: "if you see this you way over your head"
+      })
+    );
+});
 
- // get 
- router.get("/", (req, res) => {
-    Actions.get()
-      .then((data) => res.json(data))
-      .catch((data) =>
-        res.status(500).json({
-          errorMessage: "if you see this you way over your head"
-        })
-      );
-  });
- 
+router.post("/", (req, res) => {
+  const name = req.body;
+  Actions.insert(name)
+    .then((data) => {
+      Actions.get().then((data) => res.status(201).json(data));
+    })
+    .catch((error) =>
+      res.status(500).json({
+        errorMessage: "Reload the ting"
+      })
+    );
+});
+
 module.exports = router;
--- projects/projectRouter.js
+++ projects/projectRouter.js
@@ -5,14 +5,25 @@ const router = express.Router();
 router.use(express.json());
 
 router.get("/", (req, res) => {
-    Projects.get()
-      .then((data) => res.json(data))
-      .catch((data) =>
+  Projects.get()
+    .then((data) => res.json(data))
+    .catch((data) =>
+      res.status(500).json({
+        errorMessage: "Go back mate !!"
+      })
+    );
+});
+router.post("/", (req, res) => {
+    const name = req.body;
+    Projects.insert(name)
+      .then((data) => {
+        Projects.get().then((data) => res.status(201).json(data));
+      })
+      .catch((error) =>
         res.status(500).json({
-          errorMessage: "Go back mate !!"
+          errorMessage: "Reload the ting"
         })
       );
   });
- 
 
 module.exports = router;
